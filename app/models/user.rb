1a3c3af9525a110a1e7c8f9ff21d4527795f8c1e
lujanfernaud <lujanfernaud@gmail.com>
2017-08-17 12:06:31 +0100

    Add automatic 'remember me' feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,6 +30,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_path
+
+    # Simulate user clicking on 'Log out' in second window.
+    delete logout_path
+
     follow_redirect!
     assert_template "static_pages/home"
     assert_select "a[href=?]", login_path
