0c88d66c04f88ead48fbce3daddc11e6c3a00580
lujanfernaud <lujanfernaud@gmail.com>
2017-08-18 18:44:34 +0100

    Add admin role

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+User.create!(name:  "Admin",
+             email: "admin@example.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
 101.times do |n|
   name  = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
