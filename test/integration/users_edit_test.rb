051483ed74c532523ebce3e992c79c209e47fea2
lujanfernaud <lujanfernaud@gmail.com>
2017-08-18 13:53:20 +0100

    Disallow users from editing other users' profiles

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:dummy)
+    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
@@ -35,5 +36,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                       email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
