dc5638cc4fae84f524dc786ffb002c4e4becebc1
lujanfernaud <lujanfernaud@gmail.com>
2017-08-21 16:19:58 +0100

    Add activation to seeds and user fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Admin",
              email: "admin@example.com",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 101.times do |n|
   name  = Faker::Name.name
@@ -11,5 +13,7 @@ User.create!(name:  "Admin",
   User.create!(name:  name,
                email: email,
                password:              "password",
-               password_confirmation: "password")
+               password_confirmation: "password",
+               activated: true,
+               activated_at: Time.zone.now)
 end
