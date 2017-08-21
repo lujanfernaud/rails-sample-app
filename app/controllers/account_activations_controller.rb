4d9d662e4c61ead94f712365adab94357665c35a
lujanfernaud <lujanfernaud@gmail.com>
2017-08-21 20:51:11 +0100

    Add #edit action to AccountActivationsController

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,23 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    activation_token = params[:id]
+
+    if user_and_not_activated?(user, :activation, activation_token)
+      user.activate
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link."
+      redirect_to root_url
+    end
+  end
+
+  private
+
+    def user_and_not_activated?(user, activation, activation_token)
+      user && !user.activated? &&
+        user.authenticated?(activation, activation_token)
+    end
 end
