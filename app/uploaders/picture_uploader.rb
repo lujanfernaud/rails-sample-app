c28e8b35f670f4fb3c2835f69f79395d3e12fa9b
lujanfernaud <lujanfernaud@gmail.com>
2017-08-31 19:15:05 +0100

    Comment out Fog in production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,11 +7,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
+
+  # Comment out Fog in production for the time being.
+  # if Rails.env.production?
+    # storage :fog
+  # else
     storage :file
-  end
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
