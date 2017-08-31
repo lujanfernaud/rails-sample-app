c026e7bf166e3aa64380364636a8ad895a4e894c
lujanfernaud <lujanfernaud@gmail.com>
2017-08-31 07:46:24 +0100

    Add Micropost model

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:dummy)
+    @post = @user.microposts.build(content: "This is a test post.")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.user_id
+  end
+
+  test "content should be present" do
+    @post.content = "  "
+    assert_not @post.valid?
+  end
+
+  test "content should be no longer than 140 characters" do
+    @post.content = "a" * 141
+    assert_not @post.valid?
+  end
+end
